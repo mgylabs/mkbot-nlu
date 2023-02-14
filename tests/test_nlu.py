c5ecc097343586036dc1bd238148b05f7548fa5d
Mycroft Kang <thkang0629@gmail.com>
2023-02-14 17:57:09 +0900

    Update test_nlu.py

--- tests/test_nlu.py
+++ tests/test_nlu.py
@@ -6,13 +6,25 @@ from mkbot_nlu.nlu import MKBotNLU
 def test_sync_parse(nlu: MKBotNLU):
     text = "안녕하세요"
     intent = nlu.sync_parse(text)
-    intent.name == "chat::greet"
-    intent.text == text
+    assert intent.name == "chat::greet"
+    assert intent.text == text
 
 
 @pytest.mark.asyncio
 async def test_parse(nlu: MKBotNLU):
     text = "안녕하세요"
     intent = await nlu.parse(text)
-    intent.name == "chat::greet"
-    intent.text == text
+    assert intent.name == "chat::greet"
+    assert intent.text == text
+
+
+@pytest.mark.asyncio
+async def test_get_an_entity(nlu: MKBotNLU):
+    text = "메시지 4개 삭제"
+    intent = await nlu.parse(text)
+    assert intent.name == "command::delete"
+    assert intent.text == text
+
+    amount = intent.get_an_entity("amount")
+
+    assert amount == "4"
